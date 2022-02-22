ef2d8031da340565a287f3aeaf086478270983f0
Adriana Čejka <adriana.cejka@seznam.cz>
2022-02-22 23:00:31 +0100

    final

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-// tady je místo pro náš program
+
+
+function hodKostkou() {
+
+    let cislo = Math.floor(Math.random() * 6 + 1);
+    console.log(cislo);
+
+    let kostka = document.querySelector('#kostka');
+    let audio = document.querySelector('audio');
+    
+    
+    if (cislo === 6) {
+        // document.querySelector('#zprava').textContent = '6! Vyhrál jsi!';
+        document.querySelector('#zprava').innerHTML = '<span class="sestka">6!</span> Vyhrál jsi!';
+        
+        kostka.src = 'obrazky/' + cislo + '.png';
+        audio.play();
+    } else {
+        document.querySelector('#zprava').textContent = 'Hodil jsi ' + cislo + ', házej znovu!';
+        kostka.src = 'obrazky/' + cislo + '.png';
+    } 
+    
+}
+
+    
+
+
+
+
